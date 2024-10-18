bf158ac9f9373907210ab0ba3a61f7e68da5dc23
lucas19fonseca <lucas19fonseca@gmail.com>
2024-10-17 22:24:53 -0300

    rolagem

--- script.js
+++ script.js
@@ -1,35 +1,29 @@
-//rolagem de baixo pra cima
+// Rolagem para o topo ao clicar no botão
 const button = document.getElementById('lucas');
 
-button.addEventListener('click', () => {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth' 
+if (button) { // Verificar se o botão existe
+    button.addEventListener('click', () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth' 
+        });
     });
-});
-//rolagem das Âncoras
+}
+
+// Rolagem suave ao clicar nas âncoras
 document.querySelectorAll('.hearder-info').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
         const href = this.getAttribute('href');
-                if (href.startsWith("#")) {
+        if (href.startsWith("#")) {
             e.preventDefault(); 
             const targetElement = document.querySelector(href);
             
             if (targetElement) { 
                 window.scrollTo({
-                    top: targetElement.offsetTop - 190, 
+                    top: targetElement.offsetTop - 190, // Ajuste para compensar header fixo
                     behavior: 'smooth' 
                 });
             }
         }
     });
 });
-
-
-
-
-
-
-
-
-
